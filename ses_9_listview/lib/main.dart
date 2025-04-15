8ad7efce9034bc3714408a47e879bf7608f8df89
alnajamulshariq <mschariquen@gmail.com>
2025-04-16 04:04:23 +0500

    Flutter: Work On LIstView

---
+++ ses_9_listview/lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var arrNames = [
+    "Shariq",
+    "Abdullah",
+    "Ali",
+    "Samad",
+    "Yasir",
+    "Hamza",
+    "Zubair",
+  ];
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ListView Widget"),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
+        ),
+
+        // Static ListView
+        body:
+        // ListView(
+        //   children: [
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 1",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 2",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 3",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 4",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 5",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 6",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 7",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 8",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 9",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 10",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 11",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 12",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 13",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 14",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         "Here is Text 15",
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // ListView.builder by item count
+        // ListView.builder(
+        //   itemBuilder: (context, index) {
+        //     return Text("Demo Data 1");
+        //   },
+        //   itemCount: 10,
+        // ),
+        // ListView by Array
+        // ListView.builder(
+        //   itemBuilder: (context, index) {
+        //     return Text(
+        //       arrNames[index],
+        //       style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //     );
+        //   },
+        //   itemCount: arrNames.length,
+        //   itemExtent: 100,
+        //   scrollDirection: Axis.horizontal,
+        // ),
+        // ListView Separated Builder
+        ListView.separated(
+          itemBuilder: (context, index) {
+            return Text(
+              arrNames[index],
+              style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+            );
+          },
+          itemCount: arrNames.length,
+          separatorBuilder: (context, index) {
+            return Divider(height: 50, thickness: 2);
+          },
+        ),
+      ),
+    ),
+  );
+}
